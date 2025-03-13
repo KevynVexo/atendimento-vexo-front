e815dfd5b9d89243b42141758f1ad4b53f9588c1
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2025-03-13 02:49:29 -0300

    Melhorando layout do login, aplicando estilização no formulario e deixando a pagina responsiva

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,41 +10,46 @@ const Login = () => {
       <Header title="Login" />
       <main className="flex-grow flex items-center justify-center">
         <div className="flex flex-wrap justify-between items-center max-w-4xl w-full p-8">
-          {/* Formulário de Login */}
-          <div className="bg-white p-8 rounded-lg shadow-md w-96 flex-form-login">
-            <form>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-medium">Email</label>
-                <input 
-                  type="email" 
-                  className="campo-formulario campo-login-senha"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-medium">Senha</label>
-                <input 
-                  type="password" 
-                  className="campo-formulario campo-login-senha"
-                />
-              </div>
+           <div className="flex-estrutura-divisao">
+              {/* Formulário de Login */}
               <div>
-                <a href="#" className="text-purple-800 text-sm block mb-4">Esqueci minha senha</a>
-                <button 
-                  type="submit" 
-                  className="w-full bg-purple-800 text-white p-3 rounded-lg text-lg font-semibold hover:bg-purple-900 transition duration-300">
-                  Entrar
-                </button>
+                  <div className="bg-white p-8 rounded-lg shadow-md w-96 flex-form-login">
+                    <form>
+                      <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-medium labelemailsenha">Email</label>
+                        <input 
+                          type="email" 
+                          className="campo-formulario campo-login-senha"
+                        />
+                      </div>
+                      <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-medium labelemailsenha">Senha</label>
+                        <input 
+                          type="password" 
+                          className="campo-formulario campo-login-senha"
+                        />
+                      </div>
+                      <div>
+                        <a href="#" className="text-purple-800 text-sm block mb-4  forgot-password">Esqueci minha senha</a>
+                        <button 
+                          type="submit" 
+                          className="botaoLogin">
+                          Login
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                  <div className="register">
+                    <div className="createAccountDiv">
+                      <a href="#" className="block text-center mt-4 text-purple-800 font-medium createaccount">Criar conta</a>
+                    </div>
+                  </div>
+                </div>
+              {/* Ilustração */}
+              <div className="hidden md:block">
+                <img src="images/image-door.webp" alt="Ilustração" className="max-w-xs"/>
               </div>
-            </form>
-            <div>
-              <a href="#" className="block text-center mt-4 text-purple-800 font-medium">Criar conta</a>
             </div>
-          </div>
-
-          {/* Ilustração */}
-          <div className="hidden md:block">
-            <img src="images/image-door.webp" alt="Ilustração" className="max-w-xs"/>
-          </div>
         </div>
       </main>
       <Footer />
